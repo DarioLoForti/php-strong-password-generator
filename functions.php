e2f53c427e2255d345a45edddc3add1fda5f5dbd
DarioLoForti <lofortidario@gmail.com>
2024-01-29 09:20:25 +0100

    aggiungo alla funzione l'obbligo di avere per forza un carateere di verso per ogni array

--- functions.php
+++ functions.php
@@ -1,22 +1,33 @@
 <?php
-// Funzione per generare una password casuale
-function generatePassword($length) {
-    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
-    $password = '';
-    $charactersLength = strlen($characters);
 
-    for ($i = 0; $i < $length; $i++) {
-        $password .= $characters[rand(0, $charactersLength - 1)];
+function generatePassword($length) {
+    // Caratteri consentiti per la password
+    $uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    $numbers = '0123456789';
+    $specialChars = '!@#$%^&*()-_+=<>?';
+
+    // Combinare tutti i tipi di caratteri
+    $allChars = $uppercase . $lowercase . $numbers . $specialChars;
+
+    // Inizializzare la password come stringa vuota
+    $password = '';
+
+    // Aggiungere almeno un carattere da ciascuna categoria
+    $password .= $uppercase[rand(0, strlen($uppercase) - 1)];
+    $password .= $lowercase[rand(0, strlen($lowercase) - 1)];
+    $password .= $numbers[rand(0, strlen($numbers) - 1)];
+    $password .= $specialChars[rand(0, strlen($specialChars) - 1)];
+
+    // Generare il resto della password
+    for ($i = strlen($password); $i < $length; $i++) {
+        $password .= $allChars[rand(0, strlen($allChars) - 1)];
     }
+
+    // Mescolare la password per rendere l'ordine casuale
+    $password = str_shuffle($password);
 
     return $password;
 }
 
-// Verifica se è stato inviato il form
-if (isset($_GET['password_length'])) {
-
-    $passwordLength = $_GET['password_length'];
-
-    $generatedPassword = generatePassword($passwordLength);
-}
 ?>
